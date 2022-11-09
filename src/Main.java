c327fe038b199bf70838259b516ec75225dd0865
AkselSundås <akselsundas@gmail.com>
2022-11-09 13:09:34 +0100

    Första commit Andra commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Checkpoint 1, Git!");
+        String x = "Hello there!";
+        System.out.println(x);
+
+
+    }
+}
